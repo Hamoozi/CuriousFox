806cbf1cc6e2c4374c19e4cdc7ce61e50afc938c
TheColorofH <82399910+TheColorofH@users.noreply.github.com>
2021-12-26 23:55:42 -0500

    Creating a window file

--- MarioGame/game.java
+++ MarioGame/game.java
@@ -3,23 +3,16 @@ import java.awt.Dimension;
 import java.awt.Canvas;
 
 
+public class game extends Canvas implements Runnable{
+    public synchronized void start(){
 
-public class game extends Canvas{
-    // setting dimensions
-    public static final int WIDTH = 100;
-    public static final int HEIGHT = 300;
-    public static final int SCALE = 4;
+    }
+    public void run(){
 
-    
-    public game(){
+        
+    }
 
-        //creating dimensions using Canvas
-        Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
-        setPreferredSize(size);
-        setMaximumSize(size);
-        setMinimumSize(size);
-
-
+    public static void main(String args[]){
 
     }
 }
