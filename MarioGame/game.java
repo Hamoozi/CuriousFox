8a482051768d8873e05c981429c2cb484642f92a
TheColorofH <82399910+TheColorofH@users.noreply.github.com>
2021-12-30 01:29:58 -0500

    Bug fixes to window

--- MarioGame/game.java
+++ MarioGame/game.java
@@ -1,17 +1,29 @@
 package MarioGame;
 import java.awt.Dimension;
 import java.awt.Canvas;
+import javax.swing.JFrame;
 
 
 
-public class game extends Canvas implements Runnable{
-    public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9;
+public class Game extends Canvas implements Runnable{
+    // public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9, SCALE = 4;
+    // public static final String title = "Super Mario bros";
+    // private Thread thread;
+    // private boolean running = false;
+    public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9, SCALE = 4;
+    public static final String TITLE = "Super Mario bros";
 
-
-    public game(){
-        Window.main(new String[0]);
+    public Game(){
+        new Window(HEIGHT, WIDTH, TITLE, this);
     }
     public synchronized void start(){
+        
+        
+
+
+    }
+    public synchronized void stop(){
+        
 
     }
     public void run(){
@@ -20,7 +32,10 @@ public class game extends Canvas implements Runnable{
     }
 
     public static void main(String args[]){
-        new game();
+
+        new Game();
+        
+        
 
     }
 }
--- MarioGame/window.java
+++ MarioGame/window.java
@@ -2,34 +2,31 @@ package MarioGame;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import MarioGame.Game;
 
 
 
 public class Window extends Canvas{
-    public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9, SCALE = 4;
-    public static final String title = "Super Mario bros";
+    // public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9, SCALE = 4;
+    // public static final String title = "Super Mario bros";
 
     // setting dimensions
     
+    
 
-    public Window(){
-        Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
-        setPreferredSize(size);
-        setMaximumSize(size);
-        setMinimumSize(size);
-        
-
-    }
-    public static void main(String[] args){
-        Window window = new Window();
-        JFrame frame = new JFrame(title);
-        frame.add(window);
+    public Window(int HEIGHT, int WIDTH, String TITLE, Game game){
+        JFrame frame = new JFrame(TITLE);
+        frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        frame.setMaximumSize(new Dimension(WIDTH, HEIGHT));
+        frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        frame.add(game);
         frame.pack();
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
+        game.start();
+        
 
     }
 
