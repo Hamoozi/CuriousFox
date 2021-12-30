175a660d736c7f282e7c7087001b1a3d932666a7
TheColorofH <82399910+TheColorofH@users.noreply.github.com>
2021-12-30 01:35:13 -0500

    Cleaning window file

--- MarioGame/window.java
+++ MarioGame/window.java
@@ -7,12 +7,6 @@ import javax.swing.JFrame;
 
 
 public class Window extends Canvas{
-    // public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9, SCALE = 4;
-    // public static final String title = "Super Mario bros";
-
-    // setting dimensions
-    
-    
 
     public Window(int HEIGHT, int WIDTH, String TITLE, Game game){
         JFrame frame = new JFrame(TITLE);
