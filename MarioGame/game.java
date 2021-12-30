279cc07463ef228fb3b74b742287f74e1d382461
TheColorofH <82399910+TheColorofH@users.noreply.github.com>
2021-12-29 19:59:04 -0500

    Created functioning window

--- MarioGame/game.java
+++ MarioGame/game.java
@@ -3,16 +3,24 @@ import java.awt.Dimension;
 import java.awt.Canvas;
 
 
+
 public class game extends Canvas implements Runnable{
+    public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9;
+
+
+    public game(){
+        Window.main(new String[0]);
+    }
     public synchronized void start(){
 
     }
     public void run(){
 
-        
+
     }
 
     public static void main(String args[]){
+        new game();
 
     }
 }
--- MarioGame/window.java
+++ MarioGame/window.java
@@ -1,27 +1,35 @@
 package MarioGame;
 import java.awt.Canvas;
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
-public class window extends Canvas{
+
+
+public class Window extends Canvas{
+    public static final int WIDTH = 640, HEIGHT  = WIDTH / 12 * 9, SCALE = 4;
+    public static final String title = "Super Mario bros";
+
     // setting dimensions
     
 
-    public window(int width, int height, String title){
+    public Window(){
+        Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+        setPreferredSize(size);
+        setMaximumSize(size);
+        setMinimumSize(size);
+        
 
+    }
+    public static void main(String[] args){
+        Window window = new Window();
         JFrame frame = new JFrame(title);
-
-        frame.setPreferredSize(new Dimension(width, height));
-        frame.setMaximumSize(new Dimension(width, height));
-        frame.setMinimumSize(new Dimension(width, height));
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(window);
+        frame.pack();
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
-        frame.add(game);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        game.start();
+
 
     }
 
